cbcdd2cd93341641e8ba48be94dd283892318ed2
Scott Lind <scttlnd@gmail.com>
2022-07-03 21:03:05 +0200

    Page transition added

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,40 @@
+import { motion, AnimatePresence } from 'framer-motion'
 import Layout from '../components/Layout/Layout'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+
+
+export default function MyApp({ Component, pageProps, router }) {
+  const routerKey = router._key
+
+  const pageAnimations = {
+    initial: {
+      opacity: 0,
+      x: 100
+    },
+    animate: {
+      opacity: 1,
+      x: 0
+    },
+    exit: {
+      opacity: 0,
+      x: -100
+    }
+  }
+
   return (
     <Layout className="content">
-      <main>
-        <Component {...pageProps} />
-      </main>
+      <AnimatePresence exitBeforeEnter>
+        <motion.main key={routerKey}
+        variants={pageAnimations} 
+        initial="initial" 
+        animate="animate"
+        exit="exit"
+        transition={{duration: 0.5}}
+        >
+          <Component {...pageProps} key={routerKey} />
+        </motion.main>
+      </AnimatePresence>
     </Layout>
   )
 }
-
-export default MyApp
